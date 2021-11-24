ae917e902052c09990a26a6c80b1357d0f463524
noah <noah.serafin04@gmail.com>
2021-11-24 16:29:17 +0000

    commit the third

--- script.js
+++ script.js
@@ -8,37 +8,42 @@ const feedback = document.querySelector("#feedback")
 const footer = document.querySelector("#footer")
 const edit = document.querySelector("#edit")
 const deleteBtn = document.querySelector("#delete")
+const resetBtn = document.querySelector("#reset")
 var finishedTasks = document.querySelectorAll("#done")
 
 
-AddBtn.addEventListener('click', AddTask())
-tickBox.addEventListener('click', markAsDone())
-deleteBtn.addEventListener('click', deleteTasks())
-edit.addEventListener('click', edit())
-
 updateFeedback = () => {
-    footer.innerHTML = "Tasks to Complete: `${tasks.length}`"
+    feedback.innerHTML = "Tasks to Complete: `${tasks.length}`"
 }
 
-AddTask = () => {
+addTask = () => {
     console.log("adding a task...")
     let newTask = input.innerHTML;
     tasksContainer.innerHTML += '<div class="task"><span id="tickBox" class="iconify" data-icon="akar-icons:box"></span><p>${newTask}</p><span id="edit" class="iconify" data-icon="mdi-light:pencil"></span></div>';
-    input.innerHTML = '';
+    input.innerHTML = '<input id="input" type="text" placeholder="Add your task here..."></input><span class="iconify" id="addTask" data-icon="mdi-light:plus-circle"></span>';
     updateFeedback()
 }
 
+//add 'done' class/id to task
 markAsDone = () => {
     this.tickBox.innerHTML = '<span id="done" class="iconify" data-icon="akar-icons:check-box"></span>'
     footer.innerHTML = '<h3>${}Task(s) selected</h3><button id="delete">Delete <span class="iconify" data-icon="akar-icons:trash-can"></span></button>'
 }
 
+//delete marked tasks
 deleteTasks = () => {
-
+    delete finishedTasks;
 }
 
 reset = () => {
-    
+    console.log("reseting...")
+    tasksContainer.innerHTML = ''
 }
 
-//<span class="iconify" data-icon="mdi-light:pencil"></span>
+AddBtn.addEventListener('click', addTask())
+resetBtn.addEventListener('click', reset())
+tickBox.addEventListener('click', markAsDone())
+deleteBtn.addEventListener('click', deleteTasks())
+edit.addEventListener('click', edit())
+
+//event listeners are executing on page load, not on click. No idea why.
